800db8aafcaed455a2a27447006fe81909d3e73d
朱春永 <82079253+zhuchunyong@users.noreply.github.com>
2022-04-20 11:16:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,29 +6,39 @@ class BuildAfterFileUpload {
     this.ssh = new NodeSSH();
   }
   apply(compiler) {
-    if (process.env.NODE_ENV === "production") {
-      compiler.hooks.afterEmit.tapAsync(
-        "BuildAfterFileUpload",
-        async (compilation, callback) => {
-          const outputPath = compilation.outputOptions.path;
-
-          await this.connectServer();
-
-          const serverDir = this.options.serverDir;
-          await this.ssh.execCommand(`rm -rf ${serverDir}/*`);
-
-          await this.uploadFiles(outputPath, serverDir);
-
-          this.ssh.dispose();
-
-          callback();
-        }
-      );
+    const production = !!this.options.production;
+    if (process.env.NODE_ENV === "development" && !production) {
+      callback && callback();
+      return;
     }
+    if (compiler.hooks?.afterEmit) {
+      compiler.hooks.afterEmit.tapAsync("BuildAfterFileUpload", async (compilation, callback) => {
+        const outputPath = compilation.outputOptions.path;
+        this.handle(outputPath);
+
+        callback && callback();
+      });
+    } else {
+      compiler.plugin("after-emit", (compilation, callback) => {
+        const outputPath = compilation.outputOptions.path;
+        this.handle(outputPath);
+        callback && callback();
+      });
+    }
+  }
+  async handle(outputPath) {
+    await this.connectServer();
+
+    const serverDir = this.options.serverDir;
+    await this.ssh.execCommand(`rm -rf ${serverDir}/*`);
+
+    await this.uploadFiles(outputPath, serverDir);
+
+    this.ssh.dispose();
   }
 
   async connectServer() {
-    const { host, username = "root", password } = this.options;
+    const { host, username, password } = this.options;
     await this.ssh.connect({
       host: host,
       username: username,
